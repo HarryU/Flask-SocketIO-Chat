9a8d84cf015420310243e803fc5a14ce1f280fbe
Uzzell, Harry <harry.uzzell@roke.co.uk>
2018-05-04 14:24:02 +0100

    renaming various components to dm or dm-online and adding simple sound playback to connected clients

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,19 +10,19 @@ def index():
     if form.validate_on_submit():
         session['name'] = form.name.data
         session['room'] = form.room.data
-        return redirect(url_for('.chat'))
+        return redirect(url_for('.dm'))
     elif request.method == 'GET':
         form.name.data = session.get('name', '')
         form.room.data = session.get('room', '')
     return render_template('index.html', form=form)
 
 
-@main.route('/chat')
-def chat():
-    """Chat room. The user's name and room must be stored in
+@main.route('/dm')
+def dm():
+    """DM room. The user's name and room must be stored in
     the session."""
     name = session.get('name', '')
     room = session.get('room', '')
     if name == '' or room == '':
         return redirect(url_for('.index'))
-    return render_template('chat.html', name=name, room=room)
+    return render_template('dm.html', name=name, room=room)
