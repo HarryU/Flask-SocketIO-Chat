da7a6266d6b7bb6364ed27e2f5b4100882809f93
dockerhost <dockerhost@dockerhost.lan>
2018-05-11 00:25:51 +0100

    fixing some issues and adding dockerfil

--- dm.py
+++ dm.py
@@ -4,4 +4,4 @@ from app import create_app, socketio
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0")
+    socketio.run(app, certfile="/etc/letsencrypt/live/153.duckdns.org/fullchain.pem", keyfile="/etc/letsencrypt/live/153.duckdns.org/privkey.pem")
